7ff948dc0ac3e26163187bba5b30448314ca85b9
Pepito Ortega <pepito.vrortega1@gmail.com>
2026-06-26 21:51:42 -0300

    exercicios da aula

---
+++ Atividade1_exercicio2.py
@@ -0,0 +1,11 @@
+#total final
+
+arroz = 24.90
+feijao = 8.50
+macarrao = 4.30
+molhotomate = 3.70
+oleosoja = 8.90
+
+soma = arroz + feijao + macarrao + molhotomate + oleosoja
+
+resp_arroz = input('Comprar arroz? (S/N) ')
